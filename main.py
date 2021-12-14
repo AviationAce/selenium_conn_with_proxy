1926185dd13eade95d045a1137d7590bc10f6e88
AviationAce <aviationace@hotmail.com>
2021-12-13 21:22:49 -0600

    gather

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # TO DO
 # build and import FireFox, Chrome and Chromium libs that return a driver object going thru my proxy
 
+import sys
+from typing import Generic
+# sys.path.insert(1, '../Common_SDER_utils')
+# sys.path.insert(1, '\\Users\\ChopperDave64\\OneDrive\\Code\\VS Code\\Common_SDER_utils\\GenericUtils.py')
 import FireFox_conn
 import GenericUtils as GU
 
@@ -10,7 +14,6 @@ def main():
     ff_driver = ff_conn.driver
     # ff_driver.get('https://www.expressvpn.com/what-is-my-ip')
     ff_driver.get('http://httpbin.org/ip')
-    GU.GenericUtils.MyPublidIP(True)
     GU.GenericUtils.SleepFor(10, 'will quit after pause')
     ff_driver.quit()
 
